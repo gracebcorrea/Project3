6606c49a19f9abb95719907cbed86ae9bcc8158b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-03 19:20:24 -0300

    Update inbox.js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -23,11 +23,45 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function Send_Mail() {
+
+
+}
+
+
+function Mailbox(){
+
+
+
+}
+
+
+function View_Email(){
+
+
+}
+
+
+
+function Archive_and_Unarchive(){
+
+
+
+}
+
+
+
+
+function Reply(){
+
+  
 }
