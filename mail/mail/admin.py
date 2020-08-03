81c3104fa0de18ae03ee406b356ea6a33850358e
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-03 17:11:53 -0300

    Update admin.py

--- mail/mail/admin.py
+++ mail/mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Email, User
+
+class Admin(admin.ModelAdmin):
+    filter_horizontal = ("mail",)
+
+admin.site.register(User)
+admin.site.register(Email)
