df9c30f37429699245f275e99e2891ed968e1b6d
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-04 21:23:08 -0300

    Update views.py

--- mail/mail/views.py
+++ mail/mail/views.py
@@ -1,4 +1,4 @@
-import json
+import json, re, requests
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
