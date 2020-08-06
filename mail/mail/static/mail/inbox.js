64ea63eb5d526ee3f9b7a8a84c1d4dbc18c2a9ba
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-06 20:35:57 -0300

    Update inbox.js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
-
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -15,7 +14,6 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#Mailbox').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -28,19 +26,10 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#Mailbox').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-
-
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><hr>`;
-  // Show the mailbox content
-  document.querySelector('#Mailbox').addEventListener( 'DOMContentLoaded', Mailbox(mailbox));
-
-
-
-
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
 
 
