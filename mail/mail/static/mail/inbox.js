73e6a182075f0a8f1cf7322a455762dac58dd302
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-07 11:28:23 -0300

    Update inbox.js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -14,6 +14,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#emails-list').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -26,6 +27,7 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#emails-list').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
 
