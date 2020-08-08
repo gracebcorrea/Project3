a10e55574f931f5babca101220b4159d3ac5fbf1
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-08 15:29:28 -0300

    Update inbox.js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => {load_mailbox('inbox');Mailbox('inbox')});
-  document.querySelector('#sent').addEventListener('click', () => {load_mailbox('sent');Mailbox('sent')});
-  document.querySelector('#archived').addEventListener('click', () => {load_mailbox('archive');Mailbox('archive')});
-  document.querySelector('#compose').addEventListener('click', compose_email);
+    // Use buttons to toggle between views
+    document.querySelector('#inbox').addEventListener('click', () => {load_mailbox('inbox'); Mailbox('inbox')} );
+    document.querySelector('#sent').addEventListener('click', () => {load_mailbox('sent'); Mailbox('sent')} );
+    document.querySelector('#archived').addEventListener('click', () => {load_mailbox('archive'); Mailbox('archive')} );
+    document.querySelector('#compose').addEventListener('click', compose_email);
 
-  // By default, load the inbox
-  load_mailbox('inbox');
+    // By default, load the inbox
+    load_mailbox('inbox');
 });
 
 function compose_email() {
@@ -22,6 +22,10 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-submit').addEventListener('click', SendMail());
+
+
 }
 
 function load_mailbox(mailbox) {
@@ -29,22 +33,13 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#emailslist').style.display = 'block';
-  document.querySelector('#emailslist').style.display = 'none';
+  document.querySelector('#emaildetail').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><hr>`;
 
 }
-
-
-function createNode(element) {
-     return document.createElement(element);
- }
-
- function append(parent, el) {
-   return parent.appendChild(el);
- }
 
 
 function Mailbox(mailbox){
@@ -54,9 +49,11 @@ function Mailbox(mailbox){
     fetch(url)
     .then((response) => response.json())
     .then(emails => {
+
       for (let e of emails) {
     // Print emails
-           alert(`${e.id}`);
+
+           div.innerHTML = `${e.id} ${e.sender} ${e.recipients} ${e.subject} ${e.timestamp} <br>`
 
     // ... do something else with emails ...
   }
@@ -65,6 +62,26 @@ function Mailbox(mailbox){
 
 
 function SendMail() {
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  const submitsend = document.querySelector('#compose-submit').onsubmit;
+
+
+  submitsend = () => {
+    fetch('/emails', {
+      method: 'POST',
+        body: JSON.stringify({
+            recipients: recipients,
+            subject: subject,
+            body:  body
+            })
+      })
+      .then(response => response.json())
+      .then(result => {
+        load_mailbox('sent')
+      })
+
 
 
 
