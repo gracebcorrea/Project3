2444b5ae585ad17b0ba5a3643ad8634c425347a6
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-08-14 14:52:23 -0300

    Update inbox.js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Use buttons to toggle between views
     document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-    // By default, load the inbox
-    load_mailbox('inbox');
-
     document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
     document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
     document.querySelector('#compose').addEventListener('click', compose_email);
 
-
+    // By default, load the inbox
+    load_mailbox('inbox');
 });
 
 function compose_email() {
